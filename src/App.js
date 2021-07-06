8831b99eb0dfb9813e6ff0c6ece495bbce86f19a
yashhguptaa1 <yashhguptaa1@gmail.com>
2021-07-06 17:30:02 +0530

    Create new user and store in firestore DB

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/Shop';
 import Header from './components/header/Header';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/SignInSignUpPage';
 
-import { auth } from './firebase/firebase.utils';
+import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
 
@@ -29,13 +29,25 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
-
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,6 +15,37 @@ const config = {
 firebase.initializeApp(config);
 
 
+//Get user UUID and store user in firebase DB only when user is signed In
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+//Create a new user using data from our user auth object
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+
+
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
