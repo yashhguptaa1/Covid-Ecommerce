6c04244879239c45c6f034d904ab7e0a5a45df11
yashhguptaa1 <yashhguptaa1@gmail.com>
2021-07-06 18:38:05 +0530

    Sign Up with Google Implemented

--- src/pages/sign-in-and-sign-up/SignInSignUpPage.jsx
+++ src/pages/sign-in-and-sign-up/SignInSignUpPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
+
+import './SignInSignUp.scss';
 
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
