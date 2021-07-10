3e4e6480f56c2d9426d5e26500513ef8d5608250
yashhguptaa1 <yashhguptaa1@gmail.com>
2021-07-10 06:49:25 +0530

    Implemented cart with actions togglecart,cartdropdown

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
-//import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/CartIcon';
+import CartDropdown from '../cart-dropdown/CartDropdown';
 
-import { connect } from 'react-redux';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './Header.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser , hidden}) => (
   <div className='header'>
-    {/* <Link className='logo-container' to='/'>
+    <Link className='logo-container' to='/'>
         <Logo className='logo' />
-      </Link> */}
+      </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
         SHOP
@@ -30,12 +31,16 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon/>
     </div>
+    {hidden ? null : <CartDropdown/> }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+//nested destructuring
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
