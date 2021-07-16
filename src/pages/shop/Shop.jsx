18287f054b46274bfe39b2a991ca987aa1293c3b
yashhguptaa1 <yashhguptaa1@gmail.com>
2021-07-16 21:02:04 +0530

    Accessing shop data using new component

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,29 +1,11 @@
-import React,{Component}from 'react';
+import React from 'react';
 
-import CollectionPreview from '../../components/collection-preview/CollectionPreview.jsx';
+import CollectionsOverview from '../../components/collection-overview/CollectionOverview'
 
-import SHOP_DATA from './shop.data.js';
-
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    
-    return (
-      <div className='shop-page'>
-          {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview/>
+  </div>
+);
 
 export default ShopPage;
